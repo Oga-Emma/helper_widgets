a75f0d11120a68b4e6251f563acd9491f6dd78e9
Oga-Emma <sevenapps2018@gmail.com>
2021-01-17 12:01:01 +0100

    Added hide keyboard functionality

---
+++ lib/hide_keyboard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HideKeyboardWidget extends StatelessWidget {
+  HideKeyboardWidget({this.child}) : assert(child != null);
+  Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(onTap: () => hideKeyboard(context), child: child);
+  }
+}
+
+hideKeyboard(BuildContext context) {
+  if (WidgetsBinding.instance.window.viewInsets.bottom > 0.0) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+      return true;
+    }
+  }
+  return false;
+}
