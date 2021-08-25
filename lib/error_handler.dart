aa965d0979bf734868e948f86f3b4292c5b0ae1c
Oga-Emma <sevenapps2018@gmail.com>
2021-08-25 21:41:32 +0100

    minor fixes

--- lib/error_handler.dart
+++ lib/error_handler.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 class ErrorHandler {
-  static String? getErrorMessage(err, [String message = "Internal Error"]) {
-    try {
-      if (err != null && err.message != null && err.message.isNotEmpty) {
-        return err.message;
-      }
-    } catch (err) {
-      debugPrint("$err");
-    }
+  static String getErrorMessage(err, [String message = "Internal Error"]) =>
+      getErrorMessage(err, message);
+}
 
-    return message;
+String getErrorMessage(err, [String message = "Internal Error"]) {
+  try {
+    if (err != null && err.message != null && err.message.isNotEmpty) {
+      return err.message;
+    }
+  } catch (err) {
+    debugPrint("$err");
   }
+
+  return message;
 }
